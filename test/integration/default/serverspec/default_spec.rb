f84a95d0e286f1236a49669473b51a889a2a7c06
Sonia Hamilton <sonia@snowfrog.net>
2016-01-28 16:00:15 +1100

    params for dynatrace_user

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -3,8 +3,10 @@ require 'serverspec'
 # Required by serverspec
 set :backend, :exec
 
+# TODO fix
 describe user('dynatrace') do
   it { should exist }
+  # TODO fix
   it { should belong_to_group 'dynatrace' }
 end
 
@@ -15,6 +17,8 @@ end
 
 describe file('/opt/dynatrace/agent') do
   it { should be_directory }
+  # TODO fix
   it { should be_owned_by 'dynatrace' }
+  # TODO fix
   it { should be_grouped_into 'dynatrace' }
 end
